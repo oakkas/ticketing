729a879e9292ca40d694d6b526c63f7881f31ee1
OGUZ AKKAS <oguz.akkas@pinnacol.com>
2020-09-09 09:39:34 -0600

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,9 @@
+const express = require('express');
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
+});
